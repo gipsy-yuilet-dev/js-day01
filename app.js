7ea2f350cfcbee4fa01c6d18e33be42bb7c39f6b
Yulieta Eyzaguirre <42111714+gipsy-yuilet-dev@users.noreply.github.com>
2025-02-26 19:26:52 -0300

    creacion de archivos bases

---
+++ app.js
@@ -0,0 +1,38 @@
+ 
+
+let numeroUn = 1
+
+let stringUn = '1'
+
+let numeroTreinta = 30
+
+let stringTreinta = '30'
+
+let numeroDiez = 10
+
+let stringDiez = '10'
+
+
+
+
+if (COMPARAR numeroUn y stringUn) {
+
+  console.log('Las variables numeroUn y stringUn tienen el mismo valor, pero tipos diferentes')
+
+} else {
+
+  console.log('Las variables numeroUn y stringUn no tienen el mismo valor')
+
+}
+
+ 
+
+if (COMPARAR numeroTreinta y stringTreinta) {
+
+  console.log('Las variables numeroTreinta y stringTreinta tienen el mismo valor y el mismo tipo')
+
+} else {
+
+  console.log('Las variables numeroTreinta y stringTreinta no tienen el mismo tipo')
+
+}
